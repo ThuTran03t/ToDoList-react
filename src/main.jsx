b87686694ff7dad38611b3fc4c6d0f01e7dce75f
Hồ Thị Thu Trân <hothithutran03t@gmail.com>
2023-12-27 10:39:40 +0700

    update backgr - tran

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,8 @@ import '../src/styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <div className="App bg-gradient-to-r from-blue-200  to-orange-300 " > */}
+    <div className="App bg-gradient-to-r from-indigo-100 via-purple-100 to-pink-100 ... " >
     <App />
-    {/* </div> */}
+    </div>
   </React.StrictMode>,
 )
